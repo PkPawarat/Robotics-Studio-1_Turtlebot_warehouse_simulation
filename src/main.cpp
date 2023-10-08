4c21d5b599bb1091ccb1309beb210f428ff62d0b
PkPawarat <popawarat@gmail.com>
2023-10-08 12:20:19 +1100

    Update main file and new map

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,7 @@
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
 
 ros::Publisher initialPosePublisher;
+ros::Subscriber odom;
 
 void odomCallback(const nav_msgs::Odometry::ConstPtr& odomMsg) {
     // Create a message to publish to /initialpose
@@ -22,7 +23,8 @@ void odomCallback(const nav_msgs::Odometry::ConstPtr& odomMsg) {
     
     // Extract relevant data from the odomMsg and set it in initialPoseMsg
     initialPoseMsg.pose.pose = odomMsg->pose.pose;
-    
+    std::string map = "map";
+    initialPoseMsg.header.frame_id = map;
     // Publish the initial pose message
     initialPosePublisher.publish(initialPoseMsg);
 
@@ -42,13 +44,15 @@ void odomCallback(const nav_msgs::Odometry::ConstPtr& odomMsg) {
 //     return 0;
 // }
 
-
+// Test initial pose 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "odom_to_initialpose");
     ros::NodeHandle nh;
     // Create a publisher for the /initialpose topic
+    odom = nh.subscribe("/odom", 1, &odomCallback);
     initialPosePublisher = nh.advertise<geometry_msgs::PoseWithCovarianceStamped>("/initialpose", 1);
     // Spin to process incoming messages
-    ros::spinOnce(); // Process messages once
+    ros::spin();
+    ros::shutdown();
     return 0;
 }
