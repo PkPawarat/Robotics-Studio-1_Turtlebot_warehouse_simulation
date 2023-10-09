2149465dc136ea63038a1bcf0bf89add19233617
zum78 <maxtmc@outlook.com>
2023-10-09 12:08:42 +1100

    "Controller Comments"

--- src/ControllerInterface/library/controller.h
+++ src/ControllerInterface/library/controller.h
@@ -1,3 +1,14 @@
+/**
+ * @file controller.h
+ * @author Pawarat Phatthanaphusakun, Maximilian Deda
+ * @brief This header file covers the controller class and initialises all variables and functions used within it. It is used to control the robots motion and interactions
+ * @version 0.1
+ * @date 2023-10-08
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
 #ifndef CONTROLLER
 #define CONTROLLER
 
@@ -20,27 +31,121 @@
 #include "sensor_msgs/LaserScan.h"
 #include "geometry_msgs/PoseArray.h"
 
+/**
+ * @brief Initialises the Controller Class as a subsclass from the Parent ControllerInterface Class
+ * 
+ */
 class Controller : public ControllerInterface {
     private:
+        /**
+         * @brief Initialises the sensor variable
+         * 
+         */
         Sensor sensor;
+
+        /**
+         * @brief Initialises the targetDetected variable as a boolean
+         * 
+         */
         bool targetDetected;
+
+        /**
+         * @brief Initialises the qrCodeDetected variable as a boolean
+         * 
+         */
         bool qrCodeDetected;
+
+        /**
+         * @brief Initialises the obstacleDetected variable as a boolean
+         * 
+         */
         bool obstacleDetected;
+
+        /**
+         * @brief Initialises the batteryLevel variable as a boolean
+         * 
+         */
         bool batteryLevel;
+
+        /**
+         * @brief Initialises the currentTarget variable as a string
+         * 
+         */
         std::string currentTarget;
 
     public:
+        /**
+         * @brief Construct a new Controller object
+         * 
+         */
         Controller();
+
+        /**
+         * @brief Initialises the Execute function
+         * 
+         */
         virtual void Execute();
+
+        /**
+         * @brief Initialises the CheckTarget function
+         * 
+         */
         virtual void CheckTarget();
+
+        /**
+         * @brief Initialises the AssignTarget function
+         * 
+         * @param target Creates a constant string variable "target" for use in the function
+         */
         virtual void AssignTarget(const std::string& target);
+
+        /**
+         * @brief Initialises the CheckQRCode function
+         * 
+         */
         virtual void CheckQRCode();
+
+        /**
+         * @brief Initialises the DriveTo function
+         * 
+         * @param location Creates a constant string variable "location" for use in the function
+         */
         virtual void DriveTo(const std::string& location);
+
+        /**
+         * @brief Initialises the PickUpTarget function
+         * 
+         */
         virtual void PickUpTarget();
+
+        /**
+         * @brief Initialises the DropTarget function
+         * 
+         */
         virtual void DropTarget();
+
+        /**
+         * @brief Initialises the CheckObstacle function
+         * 
+         */
         virtual void CheckObstacle();
+
+        /**
+         * @brief Initialises the CheckBattery function
+         * 
+         */
         virtual void CheckBattery();
+
+        /**
+         * @brief Initialises the Stop function
+         * 
+         */
         virtual void Stop();
+
+        /**
+         * @brief Initialises the Charge function
+         * 
+         */
         virtual void Charge();
     };
 
